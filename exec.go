5e5610051b8a75f9121ecfbc9c766e79b2758232
lyderic <lyderic@localhost>
2018-03-16 09:24:46 +0000

    added exec

---
+++ exec.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func execute(command string, args ...string) {
+	cmd := exec.Command(command, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
