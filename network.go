d524f2b2ae89f822dc707de47277816263a03f29
lyderic <lyderic@nuc.lyderic.net>
2018-03-22 20:45:09 +0000

    bugs in network tools

--- network.go
+++ network.go
@@ -1,10 +1,11 @@
 package tools
 
 import (
+  "fmt"
 	"net"
 )
 
-func TcpLocalPortIsOpen(int port) bool {
+func TcpLocalPortIsOpen(port int) bool {
 	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
 		return false
