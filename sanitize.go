0862c5d238d6dcf85d01f81578a5a1787efc3d0f
lyderic <lyderic@nuc.lyderic.net>
2018-11-21 17:55:38 +0000

    sanitize: now comparing and writing to file only if needed

--- sanitize.go
+++ sanitize.go
@@ -1,7 +1,6 @@
 package tools
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,12 +10,10 @@ import (
 )
 
 func Sanitize(path string, verbose bool) (err error) {
-	start := time.Now()
-	var nbytes int
 	if _, err = os.Stat(path); os.IsNotExist(err) {
 		return fmt.Errorf("cannot sanitize %s: file not found", path)
 	}
-	if nbytes, err = substitute(path,
+	return substitute(path, verbose,
 		"---", "***",
 		"--", "—",
 		"...", "…",
@@ -27,32 +24,26 @@ func Sanitize(path string, verbose bool) (err error) {
 		" ;", " ;",
 		" :", " :",
 		" \n", "\n",
-		"  ", " "); err != nil {
-		return err
-	}
-	if verbose {
-		fmt.Printf("%s: processed %d bytes in %s\n", filepath.Base(path), nbytes, time.Since(start))
-	}
-	return
+		"  ", " ")
 }
 
-func substitute(path string, replacements ...string) (n int, err error) {
-	r := strings.NewReplacer(replacements...)
+func substitute(path string, verbose bool, replacements ...string) (err error) {
+	start := time.Now()
+	replacer := strings.NewReplacer(replacements...)
 	// get content of the file
-	var content []byte
-	if content, err = ioutil.ReadFile(path); err != nil {
-		return n, err
+	var buffer []byte
+	if buffer, err = ioutil.ReadFile(path); err != nil {
+		return
 	}
-	// create file to write to
-	var f *os.File
-	if f, err = os.Create(path); err != nil {
-		return n, err
+	content := string(buffer)
+	// Apply the replacements
+	replaced := replacer.Replace(content)
+	// If content and replaced differ, write file and notify (if verbose)
+	if replaced != content {
+		ioutil.WriteFile(path, []byte(replaced), 0644)
+		if verbose {
+			fmt.Printf("%s: processed in %s\n", filepath.Base(path), time.Since(start))
+		}
 	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
-	if n, err = r.WriteString(w, string(content)); err != nil {
-		return n, err
-	}
-	w.Flush()
 	return
 }
