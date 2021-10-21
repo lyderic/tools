0214fa75a9f59c191cdd4ddc8068ce3b751cead3
lyderic <lyderic@tintin.lyderic.net>
2021-10-21 08:28:10 +0200

    no more full path in error

--- errors.go
+++ errors.go
@@ -2,13 +2,14 @@ package tools
 
 import (
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
 func E(err error) {
 	if err != nil {
 		_, fn, line, _ := runtime.Caller(1)
-		Red("FATAL! %s:%d %v\n", fn, line, err)
+		Red("FATAL! %s:%d %v\n", filepath.Base(fn), line, err)
 		os.Exit(42)
 	}
 }
