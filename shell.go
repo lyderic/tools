1faeb91d25c1ba90701309c719a02ac6f8da645d
lyderic <lyderic@tintin.lyderic.net>
2022-06-30 09:53:07 +0200

    added capture possibility to BashExec, just in case...

--- shell.go
+++ shell.go
@@ -15,3 +15,16 @@ func BashExec(script string, args ...string) error {
 	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 	return cmd.Run()
 }
+
+func BashExecCapture(script string, args ...string) (stdout, stderr []byte, err error) {
+	if len(args) > 0 {
+		args = append([]string{"-s"}, args...) // prepending '-s'
+	}
+	cmd := exec.Command("bash", args...)
+	cmd.Stdin = bytes.NewBufferString(script)
+	var outBuffer, errBuffer bytes.Buffer
+	cmd.Stdout, cmd.Stderr = &outBuffer, &errBuffer
+	err = cmd.Run()
+	stdout, stderr = outBuffer.Bytes(), errBuffer.Bytes()
+	return
+}
--- shell_test.go
+++ shell_test.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,10 +15,34 @@ func TestBashExec(t *testing.T) {
 	}
 }
 
+func TestBashExecOutput(t *testing.T) {
+	stdout, stderr, err := BashExecCapture(scriptOutput, "hello output")
+	if err != nil {
+		t.Errorf("script %q failed! %v\n", script, err)
+	}
+	fmt.Printf("OUT [%d]> %q\n", len(stdout), string(stdout))
+	fmt.Printf("ERR [%d]> %q\n", len(stderr), string(stderr))
+}
+
+/*
+func TestLongOutput(t *testing.T) {
+	var err error
+	if err = BashExec("ls -R /"); err != nil {
+		t.Errorf("script %q failed! %v\n", script, err)
+	}
+}
+*/
+
 var script = `#!/bin/bash
 
 echo -n "Date: "
 date +%F
 echo "Input: ${1}"
 echo "@=${@}"
+`
+
+var scriptOutput = `#!/bin/bash
+
+echo $@
+echo $@ >&2
 `
